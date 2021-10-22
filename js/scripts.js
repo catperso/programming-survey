9e0ecd001a863064ddb634514cbad950f544faf8
Matt Casperson <mc.casperson@gmail.com>
2021-10-22 11:52:51 -0700

    change html/css to display results info instead of js

--- js/scripts.js
+++ js/scripts.js
@@ -22,20 +22,25 @@ $(document).ready(function() {
     $("#results").show();
     $("body").removeClass();
     $(".card-img-top").hide();
+    $(".results-info").hide();
 
     if (withHonor(favLang, favDate, whyDate, sweetroll)) {
       $("#results-language").text('KLINGONESE!');
-      $("#results-info").text("nIb quvmoH chaw nuQ pIqaD way mIQ SeHlaw tungHa' taS, ghor jatIh wanI' pab wuv qugh ghoma' jIv. Saqghom mupwI' betleH bey' ghem lupDujHom van Dub neb qaywI' loghqam van, ghob pegh noD muH run Do Qe' tlhay' chor bargh nuQ, HISlaH vIng yuQHom choba' Qol nIb mIQvaD van. SIbI' ghoma' HaD wanI' qaywI' toq roS vulqan, tlham yIt voQ Hoqra' SIm Qaw, maj 'eb qIvon HIch DIj puq poH. So qeq nuH loghqam 'ov yIt qImHa', pegh qumwI' matlh janluq pIqarD Saj ray', chor bargh to'waQvaD He pegh. taH Hurgh nem SoSbor'a' Qan lIghongan SIbI' jey' 'eSpanya Hoqra' meQ, chergh tengchaH van chergh may' Huj nagh DIr qorgh SIm. maj poQ poj woj chor bargh betleH bey' lev maj nISwI' HIch jIj, chaj Dum poQ qegh vemmoH lengwI' ghubDaQ Hurgh, neSlo' He leH ghew baQ yIt rejmorgh.");
+      $("#gowron-info").show();
       $("body").addClass("gowron");
       $("#gowron-logo").show();
     } else if (favLang === 1) {
       $("#results-language").text('Ruby!');
-      $("#results-info").text('ruby lorem ipsum');
+      $("#results-info").text("Ruby is an object-oriented scripting language. First released in 1995, it steadily grew in popularity until mass acceptance in 2006. A large amount of its popularity comes from Ruby on Rails, a framework for building modern web applications. Some of its identifying ");
       $("#ruby-logo").show();
     } else if (favLang === 2) {
       $("#results-language").text('C#!');
       $("#results-info").text('c# lorem ipsum');
       $("#csharp-logo").show();
+    } else if (favLang === 3) {
+      $("#results-language").text('JavaScript!');
+      $("#results-info").text('javascript lorem ipsum');
+      $("#javascript-logo").show();
     } else if (favLang === 4) {
       $("#results-language").text('Go!');
       $("#results-info").text('go lorem ipsum');
