a91f63ac8e5cd75f33a385fd7edc09b6c4b08cb4
Matt Casperson <mc.casperson@gmail.com>
2021-10-22 10:17:03 -0700

    add placeholders for results based on survey input

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
 /*
   To get today's date:
-  let today = new Date();
+  let fullDate = new Date();
 
-  let date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+  let today = fullDate.getFullYear()+'-'+(fullDate.getMonth()+1)+'-'+fullDate.getDate();
 
   if they select today's date, this = true
-  date === $("#surv-date").val();
+  today === $("#surv-date").val();
 
   check why today is their favourite date = true (regardless of casing)
   $("#surv-why").val().toUpperCase === 'IT IS A GOOD DAY TO DIE!'
@@ -14,13 +14,47 @@
   $("input:radio[name=action]:checked").val() === 'combat'
 */
 
+function withHonor(lang, date, why, sweetroll) {
+  return false;
+}
+
 $(document).ready(function() {
   $("#survey").submit(function(event) {
     event.preventDefault();
     
-    const favLanguage = parseInt($("#surv-lang").val());
+    const favLang = parseInt($("#surv-lang").val());
     const favDate = $("#surv-date").val();
     const whyDate = $("#surv-why").val();
     const sweetroll = $("input:radio[name=action]:checked").val();
+    const colour = $("#surv-colour").val();
+
+    $("#results").show();
+
+    if (withHonor(favLang, favDate, whyDate, sweetroll)) {
+      $("#results-language").text('KLINGON!');
+      $("#results-info").text('klingon lorem ipsum');
+      $("body").addClass("gowron");
+    } else if (favLang === 1) {
+      $("#results-language").text('Ruby!');
+      $("#results-info").text('ruby lorem ipsum');
+    } else if (favLang === 2) {
+      $("#results-language").text('C#!');
+      $("#results-info").text('c# lorem ipsum');
+    } else if (favLang === 4) {
+      $("#results-language").text('Go!');
+      $("#results-info").text('go lorem ipsum');
+    } else if (favLang === 5) {
+      $("#results-language").text('Python!');
+      $("#results-info").text('python lorem ipsum');
+    } else if (favLang === 6) {
+      $("#results-language").text('Rust!');
+      $("#results-info").text('rust lorem ipsum');
+    } else if (favLang === 7) {
+      $("#results-language").text('Swift!');
+      $("#results-info").text('swift lorem ipsum');
+    } else {
+      $("#results-language").text('JavaScript!');
+      $("#results-info").text('javascript lorem ipsum');
+    }
   });
 });
